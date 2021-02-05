74004dc2d1db0efba55406aeef20916ad3c356a1
Victor <vitopcg@gmail.com>
2021-02-05 11:58:40 +0000

    last commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://my-json-server.typicode.com/victorgonzalezz/api-synvios-web',
+  
 });
 
 export default api;
